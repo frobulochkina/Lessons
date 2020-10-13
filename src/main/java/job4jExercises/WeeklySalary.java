144d028ac3034068f5647d6d957f8a1462d12e67
Frosya Bulochkna <konyakova.elena@gmail.com>
2020-10-13 14:46:22 +0300

    WeeklySalary

---
+++ src/main/java/job4jExercises/WeeklySalary.java
@@ -0,0 +1,13 @@
+package job4jExercises;
+
+public class WeeklySalary {
+    public static int calculate(int[] hours) {
+        int weekdays = 0;
+        int weekends = 0;
+        for (int i = 0; i < 5; i++) {
+           if (int[i] ) {
+           }
+        }
+        return 0;
+    }
+}
