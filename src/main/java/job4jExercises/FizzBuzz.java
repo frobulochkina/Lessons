0c31af98baa84d7aeec8d7fba5af649cfa4f0794
Frosya Bulochkna <konyakova.elena@gmail.com>
2020-10-13 14:42:34 +0300

    FizzBuzz

---
+++ src/main/java/job4jExercises/FizzBuzz.java
@@ -0,0 +1,19 @@
+package job4jExercises;
+
+public class FizzBuzz {
+    public static String checkNumber(int number) {
+        if (number % 3 == 0 && number % 5 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else
+            return "Operation not support";
+    }
+
+    public static void main(String[] args) {
+        String rsl = FizzBuzz.checkNumber(15);
+        System.out.println(rsl);
+    }
+}
