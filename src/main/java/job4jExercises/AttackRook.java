77306373a17628b3c2b5db527c2c1807a3af8317
Frosya Bulochkna <konyakova.elena@gmail.com>
2020-10-17 14:26:27 +0300

    AttackRook

---
+++ src/main/java/job4jExercises/AttackRook.java
@@ -0,0 +1,17 @@
+package job4jExercises;
+
+public class AttackRook {
+    public static boolean check(String left, String right) {
+        if (left.charAt(0) == right.charAt(0)) {
+            return true;
+        } else if (left.charAt(1) == right.charAt(1)) {
+            return true;
+        } else
+        return false;
+    }
+
+    public static void main(String[] args) {
+        boolean rsl = AttackRook.check("B2", "A2");
+        System.out.println(rsl);
+    }
+}
